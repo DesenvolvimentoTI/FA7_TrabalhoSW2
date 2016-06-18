fec68564c49f5bbe4df8b27772f89165efc1bd6a
DesenvolvimentoTI <osmarfilho6@gmail.com>
2016-06-18 15:34:05 -0300

    Inicio do versionamento

--- ForcaVenda/models/Cliente.cs
+++ ForcaVenda/models/Cliente.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq.Mapping;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ForcaVenda.models
 {
+    [Table]
     class Cliente
     {
+        [Column]
+        int IdCliente;
+
+
+
     }
 }
