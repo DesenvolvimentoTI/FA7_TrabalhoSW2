67c7baacd274172b5e99e60766c0bdfc95aeea39
Vianney Ribeiro <Vianney Ribeiro@DESKTOP-9NGQ5TH>
2016-07-02 10:24:57 -0300

    Mapa

--- ForcaVenda/views/Pedido.xaml.cs
+++ ForcaVenda/views/Pedido.xaml.cs
@@ -10,6 +10,9 @@ using Microsoft.Phone.Shell;
 using ForcaVenda.Resources;
 using ForcaVenda.models;
 using Windows.Devices.Geolocation;
+using Microsoft.Phone.Maps.Controls;
+using System.Windows.Media.Imaging;
+using System.Device.Location;
 
 namespace ForcaVenda.views
 {
@@ -20,12 +23,13 @@ namespace ForcaVenda.views
         {
             InitializeComponent();
             CarregarClientes();
+            CarregaMapa();
         }
 
         void CarregarClientes()
         {
             txtData.Text = String.Format("{0:dd/MM/yyyy}",  DateTime.Today) ;
-            geo
+            
             using (var bd = new BancoDados())
             {
                 var clientes = (from cliente in bd.TbCliente
@@ -51,7 +55,7 @@ namespace ForcaVenda.views
             */
 
             int novoPedido;
-            Geolocator geo = new Geolocator();
+            
 
             using (var bd = new BancoDados())
             {
@@ -60,12 +64,10 @@ namespace ForcaVenda.views
                 pedido.DataPedido = Convert.ToDateTime(txtData.Text);
                 pedido.IdCliente = ((Cliente)listaClientes.SelectedItem).IdCliente;
 
-                if (geo.LocationStatus != PositionStatus.Disabled)
-                {
-                    Geoposition pos = await geo.GetGeopositionAsync();
-                    pedido.Latitude = pos.Coordinate.Latitude;
-                    pedido.Longitude = pos.Coordinate.Longitude;
-                }
+               
+                    pedido.Latitude = localPedido.Center.Latitude;
+                    pedido.Longitude = localPedido.Center.Longitude;
+                
 
                 bd.TbPedido.InsertOnSubmit(pedido);
                 bd.SubmitChanges();
@@ -75,6 +77,42 @@ namespace ForcaVenda.views
             }
 
             NavigationService.Navigate(new Uri(string.Format("/views/PedidoItem.xaml?pedido={0}", novoPedido), UriKind.Relative));
+        }
+
+        public async void CarregaMapa()
+        {
+            Geolocator geo = new Geolocator();
+
+
+
+
+
+            if (geo.LocationStatus != PositionStatus.Disabled)
+            {
+                Geoposition pos = await geo.GetGeopositionAsync();
+              
+
+                MapOverlay MyOverlay = new MapOverlay();
+                Image image = new Image();
+                image.Height = 100;
+                image.Width = 100;
+                image.Source = new BitmapImage(new Uri("/Assets/pin.png", UriKind.Relative));
+                MyOverlay.Content = image;
+                MyOverlay.GeoCoordinate = new GeoCoordinate(pos.Coordinate.Latitude, pos.Coordinate.Longitude);
+
+                MapLayer MyLayer = new MapLayer();
+                MyLayer.Add(MyOverlay);
+
+                localPedido.Center = MyOverlay.GeoCoordinate;
+                localPedido.ZoomLevel = 15;
+                localPedido.Layers.Clear();
+                localPedido.Layers.Add(MyLayer);
+            }
+
+
+
+
+
         }
 
     }
